5156d9127b56ea69c0e3c34b41f2fa890e614c7f
Baker Franke <bfranke@Bakers-MacBook-Pro-4.local>
2017-01-03 09:04:34 -0600

    cleaned up apml.js

--- apml/apml.js
+++ apml/apml.js
@@ -5,7 +5,7 @@ samples.robot = '[MOVE_FORWARD]\n[REPEAT 4 TIMES\n   [[TURN_LEFT]\n[MOVE_FORWARD
 samples.foreach = '[FOR EACH item IN list\n  [[DISPLAY |item|]\n[IF (item = \"needle\")\n  [[DISPLAY |\"Found!\"|]]\n]\n]]';
 samples.procedure = '[PROCEDURE min |a, b, c|\n  [[min <- a]\n[IF (b < min)\n  [[min <- b]]\n]\n[IF (c < min)\n  [[min <- c]]]\n[RETURN |min|]\n'
 
-var multilineBlocks = ["IF","ELSE", "REPEAT","FOR","PROCEDURE"];
+var multilineBlocks = ["IF","ELSE", "REPEAT UNTIL","FOR","PROCEDURE"];
 
 var codeType = "ap";  //this can be "ap" or "apml"
 
@@ -26,8 +26,26 @@ convs.push({a: "[ ]{2}", b: "&nbsp;&nbsp;"});
 convs.push({a: "[\|]", b: "&nbsp;&nbsp;"});
 convs.push({a: "<-", b: "&larr;"});
 
+// BAKER IDEA:
+// grr...for AP->APML conversion why not do something like how I did APML->HTML?
+// eg. {a: "[\{]", b: "[["}
+
 
 /////// HELPER FUNCTIONS //////
+var DEBUG = false;
+var DEBUG_LEVEL = 0; // 0=always, 1=important, 2=debugging fodder
+
+function debug(msg, level){
+
+    if(typeof(level) == "undefined" ){
+      level = 2;
+    }
+
+    if(DEBUG || level <= DEBUG_LEVEL){
+      debug(msg);
+    }
+}
+
 /* 
 * Process apml and replace special chars with appropriate html blocks
 * Worth nothing: css says that styles here are <pre> style so spaces have an effect!
@@ -43,13 +61,13 @@ function convert(){  //make this be apml2html(input)
   
   for(var i=0; i<convs.length; i++){
     var reg = new RegExp(convs[i].a, "g");
-    console.log("replace "+reg.toString()+" with "+convs[i].b);
+    debug("replace "+reg.toString()+" with "+convs[i].b);
 
     input = input.replace(reg, convs[i].b);
   }
   $("#htmlOut").text("<div id='APblocks'>\n"+input+"\n</div>");
   $("#APblocks").html(input);
-  console.log("input\n "+JSON.stringify($("#code").val()));
+  debug("input\n "+JSON.stringify($("#code").val()), 0);
 }
 
 /*
@@ -89,7 +107,7 @@ function isBlockStatement(line){
 
   for(var i=0; i<multilineBlocks.length; i++){
     if(line.indexOf(multilineBlocks[i]) >= 0){
-      console.log("found block statement "+line);
+      debug("found block statement "+line);
       return true;
     }
   }
@@ -108,7 +126,7 @@ function apml2ap(code){
 
     var line = lines[i];
 
-    console.log(i+".) "+line);
+    debug(i+".) "+line);
 
 
       //figure out how to replace pipes || with proper parenstr.search(/^[ ]*$/g)s ()
@@ -144,12 +162,12 @@ function apml2ap(code){
 
     // replace with proper amount.
     if(line.indexOf("{\n")>=0){ //if I inserted a \n it's because of open curly or else indent needs to happen to remainder of line after \n
-      console.log("found line break replacing with \\n + pad >>  "+pad(indent)+"<<");
+      debug("found line break replacing with \\n + pad >>  "+pad(indent)+"<<");
       line = line.replace(/[\n]/,"\n  "+pad(indent));  //force add 2 spaces since indent hasn't increased yet.
     }
     else if(line.indexOf("}\nELSE")>=0){  //I created this above --
 
-      console.log("found ELSE  >>"+line+"<<");
+      debug("found ELSE  >>"+line+"<<");
       
       line = line.replace("}\nELSE", "}\n"+pad(indent)+"ELSE")
     }
@@ -160,14 +178,14 @@ function apml2ap(code){
     
     
 
-    console.log("\t"+ i+".) >>"+line+"<<")
+    debug("\t"+ i+".) >>"+line+"<<")
 
     // if the line only contains characters with no spaces then it's a no-param procedure call
     if(line.search(/^[A-Za-z0-9_]{1}[ ]*$/g)>=0){
 
       line = line.replace(/[ ]*$/g, "");
       line = line+" ()";
-      console.log("\tFOUND procedure call line? "+line);
+      debug("\tFOUND procedure call line? "+line);
     }
 
   
@@ -177,7 +195,7 @@ function apml2ap(code){
       out += line+"\n";
     } 
     else{
-      console.log("\tONLY SPACES!!!");
+      debug("\tONLY SPACES!!!");
     }
     // if there is an opening curly brace increase indent here to affect future lines
     if(line.indexOf("{")>=0) indent+=2;
@@ -189,84 +207,6 @@ function apml2ap(code){
 
 }
 
-
-function apml2AP_old(code){
-
-  var lines = code.split("\n");
-
-  var out = "";
-  var indent = 0; //keep track of indent depth
-
-  for(var i=0; i<lines.length; i++){
-
-    var line = lines[i];
-
-    console.log(i+".) "+line)
-
-    // first remove double brackets. Those are curly braces
-    line = line.replace(/\[ *\[/g,"{\n");
-    line = line.replace(/\] *\]/g, "}");
-
-    line = line.replace("ELSE","}\nELSE");
-
-
-    
-    
-
-    // now get rid of single brackets
-    line = line.replace(/[\[\]]/g,"");
-
-    //strip any amount of leading whitespace
-    line = line.replace(/^[ ]*/,"");
-
-    // if there is a closing curly brace de-dent immediatly
-    if(line.indexOf("}")>=0) indent-=2;
-
-    // replace with proper amount.
-    if(line.indexOf("{\n")>=0){ //if I inserted a \n it's because of open curly or else indent needs to happen to remainder of line after \n
-      console.log("found line break replacing with \\n + pad >>  "+pad(indent)+"<<");
-      line = line.replace(/[\n]/,"\n  "+pad(indent));  //force add 2 spaces since indent hasn't increased yet.
-    }
-    else if(line.indexOf("}\nELSE")>=0){  //I created this above --
-
-      console.log("found ELSE  >>"+line+"<<");
-      
-      line = line.replace("}\nELSE", "}\n"+pad(indent)+"ELSE")
-    }
-    
-    
-    
-    line = pad(indent)+line;
-    
-    
-
-    console.log("\t"+ i+".) >>"+line+"<<")
-
-    // if the line only contains characters with no spaces then it's a no-param procedure call
-    if(line.search(/^[A-Za-z0-9_]{1}[ ]*$/g)>=0){
-
-      line = line.replace(/[ ]*$/g, "");
-      line = line+" ()";
-      console.log("\tFOUND procedure call line? "+line);
-    }
-
-    //figure out how to replace pipes || with proper parenstr.search(/^[ ]*$/g)s ()
-    line = pipe2parens(line);
-
-    if(line.search(/^[ ]*$/g) == -1){ //if the line is NOT just all spaces
-      
-      out += line+"\n";
-    } 
-    else{
-      console.log("\tONLY SPACES!!!");
-    }
-    // if there is an opening curly brace increase indent here to affect future lines
-    if(line.indexOf("{")>=0) indent+=2;
-
-  }
-  return out;
-
-}
 
 function pipe2parens(line){
 
@@ -285,53 +225,22 @@ function pipe2parens(line){
 
   }
 
-  console.log("AFTER open paren procedure: "+line);
+  debug("AFTER open paren procedure: "+line);
 
   // now, remaining pipes are all closing parens
   line = line.replace(/[\|]/g,")");
 
-  console.log("AFTER close paren procedure: "+line);
+  debug("AFTER close paren procedure: "+line);
 
-
-
-  //alternate first occurance to (  then last occurance to )
-  // should deal with embeded pipes...which shouldn't happen but still.
-  // var out = str;
-  // while ( out.search(/[|]/g) >= 0){
-
-  //   var left = out.indexOf("|");
-  //   var right = out.lastIndexOf("|");
-
-  //   out = out.slice(0, left)+"("+out.slice(left+1, right)+")"+out.slice(right+1)
-
-
-  // }
-  // return out;
 
   return line;
 
 }
 
 
-function pipe2parens_old(str){
-
-  //alternate first occurance to (  then last occurance to )
-  // should deal with embeded pipes...which shouldn't happen but still.
-  var out = str;
-  while ( out.search(/[|]/g) >= 0){
-
-    var left = out.indexOf("|");
-    var right = out.lastIndexOf("|");
-
-    out = out.slice(0, left)+"("+out.slice(left+1, right)+")"+out.slice(right+1)
-
-
-  }
-  return out;
-
-}
-
 function pad(amt){
+
+  if(amt < 0) return "";
 
   return Array(amt+1).toString().replace(/,/g," ")
 }
@@ -340,7 +249,7 @@ function processProcedureCalls(line, tabs){
 
   var innerMost = line.search(/[A-z_]*[ ]*[\(][A-z 0-9_\"\|,]*[\)]/);  //find innermost pattern of PROCEDURE ()
 
-  console.log(tabs+"for >>"+line+"<< innermost starts at: "+innerMost);
+  debug(tabs+"for >>"+line+"<< innermost starts at: "+innerMost);
 
   // innerMost will be the index where the code actually starts
   // if it's indented then it's possible it's already handled with surrounding [ ], just indented.
@@ -364,7 +273,7 @@ function processProcedureCalls(line, tabs){
     line = processProcedureCalls(line, tabs+"\t");
   }
   else{
-    console.log(tabs+" Nothing to replace");
+    debug(tabs+" Nothing to replace");
   }
 
   line = line.replace(/\|[ ]*\|/g, "");
@@ -375,26 +284,25 @@ function processProcedureCalls(line, tabs){
 
 function findNoParamProcedures(line){
 
+  // ASSUMPTION: processProcedureCalls has been run first.  So foo("a") --> [foo |a|]  but foo() --> [foo]
   // find pattern: [Procedure] -- these are no-args function calls
   var loc = line.search(/[\[][A-Za-z_]{1,}[\]]/);
 
   while(loc >= 0){
-  //   console.log("FOUND PROCEDURE WITH NO PARAMS "+line+ " at loc "+loc);
 
     var firstChunk = line.slice(0,loc);
     var secondChunk = line.slice(loc+1);
 
-  //   //loc is where word starts, find word end;
+    //loc is where word starts, find word end;
     var end = secondChunk.search(/[\]]/g); // force to regex search from starting point (indexOf(exp, start) doesn't take regex)
 
 
-  //   // slice in "()"
-
+    // slice in "()"
     line = firstChunk+secondChunk.slice(0,end)+"()"+secondChunk.slice(end+1);
 
-     console.log("\tchanged to: "+line);
+     debug("\tchanged to: "+line);
 
-  //   // find next
+     // find next
      loc = line.search(/[\[][A-Za-z_]{1,}[\]]/);
 
    }
@@ -496,106 +404,12 @@ function ap2apml(code){
   return out;
 }
 
-// Takes AP-style code and parses it into Baker's apml
-// TODO:  need to parse right-side expressions on assignment...or any invocation of known commands with params
-function AP2apml_old(code){
 
-  //var lines = $("#code").val().split("\n");
-  var lines = code.split("\n");
-
-  var out = "";
-
-  var nestLevel = 0; //count nesting of blocks, use to determine whether to use indent at the end.
-
-  for(var i=0; i<lines.length; i++){
-
-    var line = lines[i];
-    console.log(lines[i]);
-
-
-
-    // figure out what to do with line contents    
-
-    //0. Find empty parens (e.g. "()") and replace with nothing
-    line = line.replace(/[ ]*\([ ]*\)/g,"");
-
-
-    //1. if it's NOT an IF or a REPEAT UNTIL then parentheses
-    // need to be turned into pipes
-    if(line.indexOf("IF") < 0 && line.indexOf("UNTIL") < 0){
-        line = line.replace(/[\(\)]/g,"|");
-    }
-
-    // TODO: if line contains pattern PROCEDURE_NAME (...)  then it needs to be surrounded with []
-
-
-    //2. preserve indentation?
-    var firstCharPos = line.search("[^ ]");
-    var indent = line.substring(0, firstCharPos ); //anything from 0 up to first non-space is indent
-
-    // TODO: 
-
-    line = line.substring(firstCharPos);
-
-    
-    // 3. figure out tokens
-
-    var beginToken = "[";  //defaults
-    var endToken = "]\n";
-
-    // if it's a block statement it cannot end with a bracket
-    if( isBlockStatement(line)){
-      endToken = "\n   ";  //NOTE: extra spaces are for proper indent of block statements on next line.
-      nestLevel++;         //   this fixes the indent problem for blocks where the first line after IF/REPEAT etc.
-                          // needs to be indented.
-    }
-
-    //line = line.replace(/[\}]/g,"]");
-    //line = line.replace(/[\{}]/g,"[");
-
-    if(line.indexOf("}") >= 0){   //if this is the closing curly for a block statement
-      if(i<lines.length-1 && lines[i+1].indexOf("ELSE")>=0){  // if next thing is an ELSE then only single-close 
-        out +="]\n";                      
-      }
-      else{
-        out+="]]\n";
-      }
-      nestLevel--;
-    }
-    else if(line.indexOf("{") >= 0){
-      out+=""
-    }
-    else if(line.search("[^ \\n]")<0){
-      out += "\n";
-    }
-    else if(line.indexOf("ELSE")>=0){
-      out += "ELSE\n   ";  //NOTE: adding indent here as part of ELSE token
-    }
-    else{
-      if(i>0 && lines[i-1].indexOf("{")>=0){
-        beginToken = "[[";
-      }
-
-      //Don't use indent if code is already nested.
-      // Initial tab is taken car of endToken from IF/REPEAT/PROCEDURE, etc.
-      if(nestLevel>0){
-        indent ="";
-      }
-
-      out+=indent+ beginToken + line + endToken;
-    }
-
-  }
-
-  //$("#dump").val(out);
-  //console.debug(out);
-  return out;
-}
 
 ////// EVENT HANDLERS //////
 $( document ).ready(function() {
 
-    console.log( "ready! adding event listeners" );
+    debug( "ready! adding event listeners" );
     document.getElementById("code").addEventListener("input", convert);
     document.getElementById("fSize").addEventListener("input", fontSize);
 
@@ -614,7 +428,7 @@ $("#fSize").change(fontSize);
 
 $("input[name='codeType']").change(function(){
 
-    console.log("changed and val is: ")
+    debug("changed and val is: ")
     codeType = $("input[name='codeType']:checked").val();
 
     toggleCodeType(codeType);
@@ -633,7 +447,7 @@ function toggleCodeType(codeType){
     }
     else{  // assumes it's apml and needs to be switched to ap
 
-      console.log("converting apml-to-AP...");
+      debug("converting apml-to-AP...");
       var ap = apml2ap($("#code").val());
 
       $("#code").val(ap);
