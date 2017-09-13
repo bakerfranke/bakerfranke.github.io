57e69754292e3aaaef03b5f127d37719466f9cd2
Baker Franke <bfranke@Bakers-MacBook-Pro-4.local>
2017-09-13 15:48:51 -0500

    added choose two option and auto question type detection

--- CSPQuestionMaker/examples.js
+++ CSPQuestionMaker/examples.js
@@ -1,7 +1,7 @@
 	var examples =[] 
 
 	examples[0] = {
-  "type": "singleSelect",
+  "qType": "singleSelect",
   "qText": "What value of A would make the output of the logic circuit `false`?\n\n\n<img src=\"https://preview.ibb.co/iWHjgv/Screen_Shot_2017_09_10_at_6_13_58_PM.png\" alt=\"Screen_Shot_2017_09_10_at_6_13_58_PM\" border=\"0\" style=\"width: 250px; margin:auto; display:block\">\n",
   "eu": "2.2 Multiple levels of abstraction are used to write programs or create other computational artifacts",
   "lo": "2.2.3 Identify multiple levels of abstractions that are used when writing programs. [P3]",
@@ -32,7 +32,7 @@
   }
 };
 	examples[1] = {
-  "type": "singleSelect",
+  "qType": "singleSelect",
   "qText": "In the following code block, assume that the variables Rainy and too_cold are boolean.\n\n    IF (NOT (Rainy OR too_cold))\n    {\n        DISPLAY(\"It's a good beach day\")\n    }\n\nWhich of the following are equivalent to the above code block?",
   "eu": "5.5 Programming uses mathematical and logical concepts.",
   "lo": "5.5.1 Employ appropriate mathematical and logical concepts in programming. [P1]",
@@ -64,7 +64,7 @@
 };
 
 examples[2] = {
-  "type": "singleSelect",
+  "qType": "singleSelect",
   "qText": "You are writing a function called `swap (list, x, y)` which will exchange the position of the two values at indexes x and y in the list.\n\nExample: before and after a call to `swap (list, 2, 3)` on the list shown below\n\n<img src=\"https://preview.ibb.co/byCrDa/Screen_Shot_2017_09_11_at_1_59_14_PM.png\" alt=\"Screen_Shot_2017_09_11_at_1_59_14_PM\" border=\"0\" style=\"display: block; margin: auto; width: 600px\">\n\nThe function header is defined below.  Choose three lines of code that will perform the swap correctly.\n\n    procedure swap (list, x, y) {\n        < MISSING CODE >\n    }",
   "eu": "5.4 Programs are developed, maintained, and used by people for different purposes.",
   "lo": "5.4.1 Evaluate the correctness of a program. [P4]",
